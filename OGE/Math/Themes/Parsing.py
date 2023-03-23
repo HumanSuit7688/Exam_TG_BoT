e646bb3994f0eca13ac6ad7c04b301e229bf017a
Ivan <ivanu1706@gmail.com>
2023-03-23 18:24:22 +1100

    Добавил папку для Информатики и Русского языка

--- OGE/Math/Themes/Parsing.py
+++ OGE/Math/Themes/Parsing.py
@@ -1,10 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
-from URLs import Url1_flat, Url1_stove, Url1_garden, Url1_travel
+from URLs import Url1_flat, Url1_stove, Url1_garden, Url1_travel, Url_1var
 
-def parsing1_flat():
-    url = Url1_flat
+def parsing_var1():
+    url = Url_1var
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'lxml')
     quotes = soup.find_all('div', class_='nobreak')
-    
+    quotes_text = []
+    for quote in quotes:
+        quotes_text.append(quote.text)
+    return quotes_text
+
+print(parsing_var1())
