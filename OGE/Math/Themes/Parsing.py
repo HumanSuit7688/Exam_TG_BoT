a93ffbb6b95c0db73e10b788022108c6e377e756
Ivan <ivanu1706@gmail.com>
2023-03-23 17:15:00 +1100

    парсер(очень очень бета версия)

--- OGE/Math/Themes/Parsing.py
+++ OGE/Math/Themes/Parsing.py
@@ -1,2 +1,10 @@
-import bs4
-import request
+from bs4 import BeautifulSoup
+import requests
+from URLs import Url1_flat, Url1_stove, Url1_garden, Url1_travel
+
+def parsing1_flat():
+    url = Url1_flat
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    quotes = soup.find_all('div', class_='nobreak')
+    
---
+++ OGE/Math/Themes/URLs.py
@@ -0,0 +1,4 @@
+Url1_flat = 'https://math-oge.sdamgia.ru/test?category_id=122&filter=all'
+Url1_garden = 'https://math-oge.sdamgia.ru/test?category_id=103&filter=all'
+Url1_travel = 'https://math-oge.sdamgia.ru/test?category_id=118&filter=all'
+Url1_stove = 'https://math-oge.sdamgia.ru/test?category_id=114&filter=all'
